e246638148ac92207bc5d7be5f60316d959327e0
Github2019 <ffpsping@163.com>
2020-10-20 11:20:49 +0800

    updating the example of this chapter

---
+++ chapter4/4_11.rs
@@ -0,0 +1,18 @@
+use std::ops::Drop;
+#[derive(Debug)]
+struct S(i32);
+impl Drop for S{
+	fn drop(&mut self){
+		println!("Drop {}",self.0);
+	}
+}
+fn main(){
+	let x=S(1);
+	println!("Create x:{:?}",x);
+	{
+		let y=S(2);
+		println!("Create y:{:?}",x);
+		println!("exit inner scope");
+	}
+	println!("exit main");
+}
